8a63da9add708dd7727991e4bae06c6196e172d2
kamranhossain <kamran.hossain227@gmail.com>
2026-04-06 02:52:54 +0600

    add .env params to config files

--- config/dev.exs
+++ config/dev.exs
@@ -2,13 +2,14 @@ import Config
 
 # Configure your database
 config :universal_enterprise_platform, UniversalEnterprisePlatform.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("DB_USER") || "postgres",
+  password: System.get_env("DB_PASSWORD") || "postgres",
   hostname: "localhost",
   database: "universal_enterprise_platform_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  pool_size: 10,
+  log_level: :debug
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :universal_enterprise_platform, UniversalEnterprisePlatform.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("DB_USER") || "postgres",
+  password: System.get_env("DB_PASSWORD") || "",
   hostname: "localhost",
   database: "universal_enterprise_platform_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
