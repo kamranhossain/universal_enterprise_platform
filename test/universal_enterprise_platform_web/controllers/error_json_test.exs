6c03e1c4275d5e51997a6ecd11c9bc0d6f0ae722
kamranhossain <kamran.hossain227@gmail.com>
2026-04-06 02:18:33 +0600

    update Readme and generate the phoenix application for universal platform

---
+++ test/universal_enterprise_platform_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule UniversalEnterprisePlatformWeb.ErrorJSONTest do
+  use UniversalEnterprisePlatformWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert UniversalEnterprisePlatformWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert UniversalEnterprisePlatformWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
